6ce023265dcd63acdbefbd1b898e9c19bd7ac3fb
Victor Henrique Souza <vhs.desenvolvedorweb@gmail.com>
2026-02-16 22:11:30 -0300

    update 2

--- index.js
+++ index.js
@@ -6,32 +6,29 @@ const fomeMedia = document.getElementById('medio')
 const fomeAlta = document.getElementById('esfomeado')
 const inputBebidas = document.getElementById('numBebidasInput')
 const inputDuracao = document.getElementById('duracao')
+const outputCarne = document.getElementById('carne')
+const outputGelo = document.getElementById('gelo')
+const outputCarvao = document.getElementById('carvao')
 
 function calcularPessoas() {
     const adultos = Number(inputAdultos.value)
     const criancas = Number(inputCriancas.value)
 
-    if (Number(inputAdultos.value) >= 2) {
-        const pessoas = adultos + (criancas / 2)
-        console.log('Tem ', pessoas, ' pessoas no churrasco')
-        return pessoas
-    } else {
-        mensagemAviso.textContent = 'Adicione mais adultos ao churrasco.'
-        return
-    }
+    const pessoas = adultos + (criancas / 2)
+    return pessoas
 }
 
 function validarCarne() {
     if (calcularPessoas() < 2) {
         mensagemAviso.textContent = 'Adicione mais adultos ao churrasco.'
-    } 
-    
-    else {
-        calcularGramas()
+        mensagemAviso.classList.add('erro')
+        return false
+    } else {
+        return calcularCarne()
     }
 }
 
-function calcularGramas() {
+function calcularCarne() {
     const pessoas = calcularPessoas()
     let carneGramas = 0;
 
@@ -42,28 +39,53 @@ function calcularGramas() {
     } else if (fomeAlta.checked) {
         carneGramas = pessoas * 600
     }
-    calcularDuracao()
     return carneGramas
 }
 
-function calcularGelo() {
-    const bebidaLitros = Number(inputBebidas.value)
-    let gelo = 0
-    
-    if (bebidaLitros < 2 || bebidaLitros === '') {
+function validarGelo() {
+    if (inputBebidas.value < 1) {
         mensagemAviso.textContent = 'Adicione mais litros de bebida.'
-        return
+        mensagemAviso.classList.add('erro')
+        return false
     } else {
-        gelo = bebidaLitros / 4
+        return calcularGelo()
     }
+}
 
+function calcularGelo() {
+    let gelo = inputBebidas.value / 4
     return gelo
+}
+
+function calcularCarvao() {
+    let carvao = calcularCarne() * 1.15
+    return carvao
+}
+
+function calcularPlanejamento() {
+    if (!validarCarne() || !validarGelo() || !calcularDuracao()) { return }
+    outputCarne.textContent = calcularCarne()
+    outputGelo.textContent = calcularGelo()
+    outputCarvao.textContent = calcularCarvao()
 }
 
 function calcularDuracao() {
     const duracao = inputDuracao.value
-
-    if (duracao > '04:00') { // continuar aqui
-
+    if (duracao === '00:00' || !duracao) {
+        mensagemAviso.textContent = 'Preencha a duração do evento.'
+        mensagemAviso.classList.add('erro')
+        return false
+    } else if (duracao < '04:00') {
+        return true
     }
+
+    const carneNova = Math.round(calcularCarne() * 1.5)
+    const geloNovo = Math.round(calcularGelo() * 1.5)
+    const carvaoNovo = Math.round(calcularCarvao() * 1.5)
+
+    outputCarne.textContent = carneNova
+    outputGelo.textContent = geloNovo
+    outputCarvao.textContent = carvaoNovo
+
+    return novasComidas
 }
